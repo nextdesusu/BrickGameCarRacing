b68a68dca742aec99d576f606559194a7748e175
nextdesusu <nextdesusu@gmail.com>
2020-07-03 00:38:20 +0400

    stye: add styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainCanvasComponent } from './main-canvas/main-canvas.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCanvasComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
